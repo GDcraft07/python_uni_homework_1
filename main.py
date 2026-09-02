8a45bdb7db686f9c9547700cdb47e28c16b1a16d
gdcraft07 <iklass987654321@gmail.com>
2026-09-02 19:36:11 +0300

    small change in main.py. hw 1.1.1

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from fraction import *
 def step():
     numerator_1 = input("Введите числитель первого числа: ")
     denominator_1 = input("Введите знаменатель первого числа: ")
+    fraction_1 = Fraction(numerator_1, denominator_1)
 
     numerator_2 = input("Введите числитель второго числа: ")
     denominator_2 = input("Введите знаменатель второго числа: ")
+    fraction_2 = Fraction(numerator_2, denominator_2)
 
     sign = input("Введите операцию: ")
-
-    fraction_1 = Fraction(numerator_1, denominator_1)
-    fraction_2 = Fraction(numerator_2, denominator_2)
 
     result = None
 
